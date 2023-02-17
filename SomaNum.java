cafc71c50df27829078ae91af30cbd99e38359ae
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-17 15:30:01 -0300

    Update SomaNum.java

--- SomaNum.java
+++ SomaNum.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class SomaNum {
 	public static void main(String[] args) {
-		int numero=0, soma=0;
+	    	int numero=0, soma=0;
 		Scanner lerDados = new Scanner(System.in);
 		
 		do {
-			soma = soma + numero;
-			System.out.println("Informe um número: ");
-			numero = lerDados.nextInt();
+		     soma = soma + numero;
+		     System.out.println("Informe um número: ");
+		     numero = lerDados.nextInt();
 		}while(numero != -1);
 		
 		System.out.printf("Resultado: %d",soma);
